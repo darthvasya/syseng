abe5dcf9c0852999886c05e6d1b3bc9dd498e2bd
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2017-01-25 18:29:49 +0300

    add DAL

---
+++ back/back.DAL/Interfaces/IClientManager.cs
@@ -0,0 +1,14 @@
+﻿using back.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace back.DAL.Interfaces
+{
+    public interface IClientManager : IDisposable
+    {
+        void Create(ClientProfile item);
+    }
+}
---
+++ back/back.DAL/Repositories/ClientManager.cs
@@ -0,0 +1,31 @@
+﻿using back.DAL.EF;
+using back.DAL.Entities;
+using back.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace back.DAL.Repositories
+{
+    public class ClientManager : IClientManager
+    {
+        public ApplicationContext Database { get; set; }
+        public ClientManager(ApplicationContext db)
+        {
+            Database = db;
+        }
+
+        public void Create(ClientProfile item)
+        {
+            Database.ClientProfiles.Add(item);
+            Database.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Database.Dispose();
+        }
+    }
+}
