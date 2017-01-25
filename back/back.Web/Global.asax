e97d93206cf91929a4f4fb5bdc852277a6440b6d
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2017-01-25 18:56:01 +0300

    initial web proj

---
+++ back/back.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="back.Web.MvcApplication" Language="C#" %>
--- back/back.Web/Properties/AssemblyInfo.cs
+++ back/back.Web/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("back")]
+[assembly: AssemblyTitle("back.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("back")]
+[assembly: AssemblyProduct("back.Web")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ac3dfd0c-6147-4657-bb0b-214a99e4c521")]
+[assembly: Guid("0854ac8c-4542-4bec-a4c4-5dbb2d911ca1")]
 
 // Version information for an assembly consists of the following four values:
 //
