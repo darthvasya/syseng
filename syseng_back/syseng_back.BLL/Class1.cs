fa637ed050ee9ef9d8f5c5f0bd0d35807907d782
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2017-01-25 17:39:56 +0300

    add some packages

---
+++ syseng_back/syseng_back.BLL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace syseng_back.BLL
+{
+    public class Class1
+    {
+    }
+}
