f8d485b3272db612fb1ec8e7179f26b579c7fbd0
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2017-01-27 18:19:26 +0300

    work with front

--- syseng_back/syseng_back.Web/App_Start/BundleConfig.cs
+++ syseng_back/syseng_back.Web/App_Start/BundleConfig.cs
@@ -5,7 +5,6 @@ namespace syseng_back.Web
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -14,8 +13,6 @@ namespace syseng_back.Web
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
@@ -23,9 +20,10 @@ namespace syseng_back.Web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/index").Include(
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/css/master.css",
+                      "~/Content/css/index-page.css"));
         }
     }
 }
