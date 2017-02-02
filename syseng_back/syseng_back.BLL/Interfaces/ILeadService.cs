509e8ae4e1c645163226202710fdd41e0e5e222a
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2017-02-02 19:31:15 +0300

    add lead service

--- syseng_back/syseng_back.BLL/Interfaces/ILeadService.cs
+++ syseng_back/syseng_back.BLL/Interfaces/ILeadService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using syseng_back.BLL.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,10 @@ namespace syseng_back.BLL.Interfaces
 {
     public interface ILeadService
     {
-
+        LeadDTO GetLead(int id);
+        IEnumerable<LeadDTO> GetLeads();
+        void AddLead(LeadDTO lead);
+        void RemoveLead(int id);
+        void Dispose();
     }
 }
---
+++ syseng_back/syseng_back.BLL/Services/LeadService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using syseng_back.BLL.DTO;
+using syseng_back.BLL.Infrastructure;
+using syseng_back.BLL.Interfaces;
+using syseng_back.DAL.Entities;
+using syseng_back.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace syseng_back.BLL.Services
+{
+    public class LeadService : ILeadService
+    {
+        IUnitOfWork _context { get; set; }
+
+        public LeadService(IUnitOfWork uow)
+        {
+            _context = uow;
+        }
+
+        public void AddLead(LeadDTO lead)
+        {
+            Mapper.Initialize(c => c.CreateMap<LeadDTO, Lead>());
+            _context.Leads.Create(Mapper.Map<LeadDTO, Lead>(lead));
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public LeadDTO GetLead(int id)
+        {
+            var lead = _context.Leads.Get(id);
+            Mapper.Initialize(c => c.CreateMap<Lead, LeadDTO>());
+            return Mapper.Map<Lead, LeadDTO>(lead);
+        }
+
+        public IEnumerable<LeadDTO> GetLeads()
+        {
+            Mapper.Initialize(c => c.CreateMap<Lead, LeadDTO>());
+            return Mapper.Map<IEnumerable<Lead>, List<LeadDTO>>(_context.Leads.GetAll());
+        }
+
+        public void RemoveLead(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
